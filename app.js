9c89cbab81c77e20f6410788458f3b6f56fcfb3c
Dev205 <dev205mg@gmail.com>
2025-02-10 16:28:58 +0300

    Contact et config api

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+import {fetchJSON} from './functions/api.js';
+import { createElement } from './functions/dom.js';
+try {
+  const todos = await fetchJSON('https://jsonplaceholder.typicode.com/todos?_limit=5');
+} catch (e) {
+  const alert = createElement('div', {
+    class: 'alert alert-danger text-center m-2',
+    role: 'alert'
+  }, "Impossible de charger les elements");
+  document.body.prepend(alert);
+}
+
 class TodoList {
   
 }
---
+++ functions/dom.js
@@ -0,0 +1,14 @@
+/**
+ * Fonction qui creer element HTML et ajout ses attributs
+ * @param {string} tagName 
+ * @param {object} attributes 
+ * @return {HTMLElement}
+ */
+export function createElement(tagName, attributes = {}, text = ''){
+  const element = document.createElement(tagName);
+  element.innerText = text;
+  for(let [attribute, value] of Object.entries(attributes)){
+    element.setAttribute(attribute, value);
+  }
+  return element;
+}
